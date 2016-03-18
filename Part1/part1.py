bc4b28b77c6dedfcefec31eb5a78474e45855b4d
ben-cooper <ben0cooper@gmail.com>
2016-03-18 19:20:13 -0400

    Implemented 2 functions in part1.

--- Part1/part1.py
+++ Part1/part1.py
@@ -2,6 +2,22 @@ import qparser
 
 stock_data = qparser.create_stocks()
 get_date = qparser.t_to_date
+N = len(stock_data[get_date(0)])
+T = len(stock_data.keys())
 
-def rcc(t, j):
-	return stock_data[get_date(t)][j].so/stock_data[get_date(t-1)][j].so-1
+def RCC(t, j):
+	return stock_data[get_date(t)][j].sc/stock_data[get_date(t-1)][j].sc-1
+
+def AvrRCC(t):
+    total = 0
+    for stock in stock_data[get_date(t)]:
+        total += stock.sc
+    return total / N
+
+def W1(t,j):
+    return -(1/N)*(RCC(t-1, j) - AvrRCC(t-1))
+
+
+
+def RP1(t):
+    return
